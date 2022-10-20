13b6d453eb91fed057594315310dc543af6736b9
kanchan <kanchankotkar001@gmail.com>
2022-10-20 18:08:02 +0530

    node js file add commit

---
+++ array-methods/array-slice.js
@@ -0,0 +1,7 @@
+// slice
+const arr=["A","B","C","D","E"];
+let picearray=arr.slice(2)
+console.log(picearray);
+
+let array=arr.slice(1,4)
+console.log(array);
--- array-methods/array-splice.js
+++ array-methods/array-splice.js
@@ -9,5 +9,13 @@ console.log(arr);
 const m=['A','B','C','D','E'];
 console.log(m);
 
-arr.splice(2,0,'kanchan')
+m.splice(2,0,'R');
 console.log(m);
+
+m.splice(2,0,'2.5');
+console.log(m);
+
+
+m.splice(5,1,'kanchan');
+console.log(m);
+
